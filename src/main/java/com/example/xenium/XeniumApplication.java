c3a3b9485ccd1e15a10892fe9ae4def48dc1f17b
cutegyuseok <yigyuseok@gmail.com>
2022-12-20 19:28:11 +0900

    create

---
+++ src/main/java/com/example/xenium/XeniumApplication.java
@@ -0,0 +1,13 @@
+package com.example.xenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XeniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XeniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xenium/XeniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XeniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
